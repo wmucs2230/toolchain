4fdca3cf9b661ab1a94da193205849126e89f66b
Chris Sphinx <2583639+chrissphinx@users.noreply.github.com>
2022-08-23 14:48:40 -0400

    Add new script and `README.md`

---
+++ README.md
@@ -0,0 +1,11 @@
+# MSP430 Toolchain for Ubuntu 20.04LTS
+
+Directions
+----------
+
+1. Click the green **Code** button and, in the dropdown, select **Download ZIP**.
+1. Save the `.zip` file to your Virtual Machine, not your host machine.
+1. Unzip the archive and either:
+    * double-click the `script.sh` file in the file browser **OR**
+    * execute `./script.sh` in the terminal
+
---
+++ script.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# install dependencies
+echo -e "\e[32m>> Update packages...\e[39m"
+sudo apt update
+echo -e "\e[32m>> Install dependencies...\e[39m"
+sudo apt install -y binutils-msp430 build-essential curl gcc-msp430 gdb-msp430 git libboost1.71-all-dev libhidapi-dev libusb-1.0-0-dev libusb-dev libreadline-dev minicom msp430-libc msp430mcu vim wget
+
+# create ~/Downloads if it doesn't exist
+mkdir -p /home/$(whoami)/Downloads
+
+# install udev rules and add user to dialout group
+echo -e "\e[32m>> Install udev rules and add user to dialout...\e[39m"
+cd /home/$(whoami)/Downloads
+chmod +x msp430uif_install.sh
+sudo ./msp430uif_install.sh --install
+sudo usermod -a -G dialout $(whoami)
+
+# install libmsp430.so from source
+echo -e "\e[32m>> Download slac460y.zip from TI...\e[39m"
+mkdir libmsp430
+cd libmsp430
+unzip -q ../slac460y.zip
+patch -p1 < ../libmsp430.patch
+echo -e "\e[32m>> Build libmsp430.so library from source...\e[39m"
+make
+echo -e "\e[32m>> Install libmsp430.so...\e[39m"
+sudo mv libmsp430.so /usr/lib
+sudo ldconfig
+
+# install mspdebug custom fork
+echo -e "\e[32m>> Build mspdebug from source...\e[39m"
+cd /home/$(whoami)/Downloads
+git clone https://github.com/chrissphinx/mspdebug.git
+cd /home/$(whoami)/Downloads/mspdebug
+make
+echo -e "\e[32m>> Install mspdebug...\e[39m"
+sudo make install
+
+# install libemb custom fork
+echo -e "\e[32m>> Build libemb from source...\e[39m"
+cd /home/$(whoami)/Downloads
+git clone https://github.com/chrissphinx/libemb.git
+cd /home/$(whoami)/Downloads/libemb
+TARCH=MSP430 make
+echo -e "\e[32m>> Install libemb...\e[39m"
+TARCH=MSP430 INSTDIR=/usr/msp430 sudo -E make install
+
+# create ~/msp430 for code and drop in test program
+echo -e "\e[32m>> Create ~/msp430 directory with hworld.c test program...\e[39m"
+mkdir -p /home/$(whoami)/msp430
+cd /home/$(whoami)/msp430
+mv /home/$(whoami)/Downloads/hworld.c .
+
+# set minicom defaults
+echo -e "\e[32m>> Set defaults from minicom...\e[39m"
+cd /home/$(whoami)/Downloads
+sudo mv minirc.dfl /etc/minicom
+
+# all done, mention restarting
+echo -e "\e[32m>> Installation complete! You should probably restart your machine\e[39m"
+
